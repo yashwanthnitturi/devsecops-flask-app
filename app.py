16dd1e332e523b00b2f5f41b6d0c8c4e34441105
Ubuntu <ubuntu@ip-10-0-1-68.ap-south-1.compute.internal>
2026-06-22 16:29:33 +0000

    GitOps deployment test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "gitOps Pipeline Running Successfully 🚀"
+    return "devsecOps Pipeline Running Successfully 🚀"
 
 @app.route("/health")
 def health():
